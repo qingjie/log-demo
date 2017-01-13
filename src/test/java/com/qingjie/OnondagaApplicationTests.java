d1cd65fbf15c0c3340635a2b26fa2761594eb740
qingjie <qzhao02@syr.edu>
2017-01-13 00:23:57 -0500

    Spring Boot中对log4j进行多环境不同日志级别的控制

--- src/test/java/com/qingjie/OnondagaApplicationTests.java
+++ src/test/java/com/qingjie/OnondagaApplicationTests.java
@@ -1,7 +1,6 @@
 package com.qingjie;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,29 +10,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.qingjie.service.CarProperties;
 
-import ch.qos.logback.classic.Logger;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnondagaApplicationTests {
-
-	private static final Log log = LogFactory.getLog(OnondagaApplicationTests.class);
+	private Logger logger = Logger.getLogger(getClass());
 
 	@Autowired
 	private CarProperties carProperties;
 
 	@Test
 	public void test1() throws Exception {
+		
+		logger.info("=====info====");
+		logger.debug("====debug====");
+		logger.error("====error====");
+		
 		Assert.assertEquals("name1", carProperties.getName());
 		Assert.assertEquals("title1", carProperties.getTitle());
 		Assert.assertEquals("name1 - title1", carProperties.getDesc());
-		
-		log.info("current env: " + carProperties.getCurrent_env());
-		log.info("Random string: " + carProperties.getValue());
-		log.info("Random int: " + carProperties.getNumber());
-		log.info("Random long: " + carProperties.getBignumber());
-		log.info("Random number in 10: " + carProperties.getTest1());
-		log.info("Random number from 10 to 20:" + carProperties.getTest2());
+
+		logger.info("current env: " + carProperties.getCurrent_env());
+		logger.info("Random string: " + carProperties.getValue());
+		logger.info("Random int: " + carProperties.getNumber());
+		logger.info("Random long: " + carProperties.getBignumber());
+		logger.info("Random number in 10: " + carProperties.getTest1());
+		logger.info("Random number from 10 to 20:" + carProperties.getTest2());
 
 	}
 
